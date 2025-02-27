0ff997bee8199cc6f7148021bbc38340a3d8c1ed
saman azadi <samanazadi1996@gmail.com>
2025-02-27 20:54:21 +0330

    'Program.cs' was modified and 'launchSettings.json' was modified and 'appsettings.json' was modified
    Modified : Program.cs
    Modified : Properties/launchSettings.json
    Modified : appsettings.json

--- Program.cs
+++ Program.cs
@@ -2,15 +2,9 @@ using Sam.CarsTelegramBot.Services.Infrastructures;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.RegisterTelegramBot(builder.Configuration);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
 
 app.MapGet("/", () => "healthy");
 
